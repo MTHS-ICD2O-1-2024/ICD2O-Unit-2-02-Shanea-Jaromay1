8abbbbfe9214573f0316fd8c25f527f677ea7c98
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-07 18:20:12 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: March 2025
 // This file contains the JS functions for index.html
 
 /**
- * Displays an alert with the message "Hello, World!"
+ * Do basic math.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function doMathClicked() {
+  document.getElementById("area-math").innerHTML = "<p>The area is: " + (5 * 3) + "cm²</p>"
+  document.getElementById("perimeter-math").innerHTML = "<p>The perimeter is: " + (2 * (5 + 3)) + "cm</p>"  
+
 }
